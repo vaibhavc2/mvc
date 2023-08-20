234f2af24d9456b223fca43096630f202fee248d
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-08-21 03:22:59 +0530

    fix engine and minor other fixes

--- data/database.js
+++ data/database.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 
-
 export const connectDB = () => {
     mongoose.connect(process.env.MONGODB_URI, {
         dbName: "mvc",
-    }).then(() => {
-        console.log("Database connected successfully");
+    }).then((c) => {
+        console.log(`Database connected successfully with host: ${c.connection.host}`);
     }).catch((err) => {
         console.log(err);
     });
